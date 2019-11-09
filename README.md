4d5ab9b012703b1fbcbb2126b810ebaa14e99ae8
ramprakashram <ram@gravityilabs.com>
2019-11-09 17:43:52 +0530

    Go News Notifier created

---
+++ README.md
@@ -0,0 +1,17 @@
+# go-news-headline
+Script to show News Headline as a notification
+
+# Running in Windows
+Execute the news.exe
+
+## Development
+1. Clone the repository
+2. Run go get -v github.com/martinlindhe/notify
+3. go run news.go
+
+## Note:
+  This script uses India as the location and category as technology, you can update that again.
+
+## Future Updates:
+1. Cron Job to display news at an specified interval
+2. Click on notification to redirect to the page
---
+++ news.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/martinlindhe/notify"
+)
+
+type ArticleSource struct {
+	Id   string
+	Name string
+}
+
+type Articles struct {
+	Source      ArticleSource
+	Author      string
+	Title       string
+	Description string
+	Url         string
+	UrlToImage  string
+	PublishedAt string
+	Content     string
+}
+
+type NewsResponse struct {
+	Status       string
+	TotalResults string
+	Articles     []Articles
+}
+
+const APP_ID = "364aa37e19b347d39d795a42f8eb7dab"
+const API = "https://newsapi.org/v2/top-headlines"
+
+func getHeadlines(countryCode string, category string, pageSize string) []Articles {
+	var newsData NewsResponse
+	apiCall := API + "?country=" + countryCode + "&category=" + category + "&pageSize=" + pageSize + "&apiKey=" + APP_ID
+	res, err := http.Get(apiCall)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+	}
+	respData, respErr := ioutil.ReadAll(res.Body)
+
+	if respErr != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", respErr)
+	}
+	// fmt.Printf("%s", respData)
+	json.Unmarshal([]byte(respData), &newsData)
+	return newsData.Articles
+}
+
+func main() {
+	articles := getHeadlines("in", "technology", "3")
+	if len(articles) > 0 {
+		news := "\n" + articles[0].Title
+		// news = news + "\n2. " + articles[1].Title
+		// news = news + "\n3. " + articles[2].Title
+		notify.Notify("Headlines", "Top Technology News", news, "")
+	} else {
+		fmt.Printf("%v", articles)
+	}
+
+}
